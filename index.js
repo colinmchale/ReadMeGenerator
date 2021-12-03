b0da6de6e4cd1c8f990c54045126c5f8542f2f68
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-12-02 23:07:56 -0600

    added template for prompt and generator function

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: "What is your name?",
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message:  "What languages do you know?",
+      name: 'languages',
+    },
+    {
+      type: 'input',
+      message: "What is your preferred method of communication?",
+      name: 'preffered',
+    },
+  ])
+  .then((response) =>
+        fs.writeFile("README2.md", readMeGenerator(response), err => console.log(err))
+  );
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-
+function readMeGenerator(response) {
+    return `## Name
+${response.username}
+    
+## Known Languages
+${response.languages}
+    
+## Preffered method of Communication
+${response.preffered}`;
+}
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
