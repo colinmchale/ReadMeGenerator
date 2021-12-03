c42b7b04b8055fbe3a49e71c1872a2d9ebdf1332
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-12-03 00:59:25 -0600

    added more to propmt and structure of ReadMe

--- index.js
+++ index.js
@@ -4,22 +4,43 @@ const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
 inquirer
   .prompt([
     {
       type: 'input',
-      message: "What is your name?",
+      message: "Please enter a Title:",
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message:  "Please enter installation instructions:",
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message:  "Please enter usage information:",
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message:  "Please enter contribution guidelines:",
+      name: 'guidelines',
+    },
+    {
+      type: 'input',
+      message: "Please enter test instructions:",
+      name: 'test',
+    },
+    {
+      type: 'input',
+      message: "What is your github username?",
       name: 'username',
     },
     {
       type: 'input',
-      message:  "What languages do you know?",
-      name: 'languages',
-    },
-    {
-      type: 'input',
-      message: "What is your preferred method of communication?",
-      name: 'preffered',
+      message: "What is your email?",
+      name: 'email',
     },
   ])
   .then((response) =>
@@ -29,14 +50,40 @@ inquirer
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 function readMeGenerator(response) {
-    return `## Name
-${response.username}
-    
-## Known Languages
-${response.languages}
-    
-## Preffered method of Communication
-${response.preffered}`;
+    return `#${response.title}
+
+##Table of Contents
+[Description](#Description)  
+[Installation](#Installation)
+[Usage](#Usage)
+[License](#License)
+[Contributing](#Contributing)
+[Tests](#Tests)
+[Questions](#Questions)
+
+##Description
+${response.description}
+
+
+##Installation
+${response.installation}
+
+##Usage
+${response.usage}
+
+##License
+${response.license}
+
+##Contributing
+${response.guidelines}
+
+##Tests
+${response.tests}
+
+##Questions
+Link to github: [Click Here](https://github.com/${response.username})
+Please submit any additional questions you may have in an email to ${response.email} 
+`   
 }
 // TODO: Create a function to initialize app
 function init() {}
