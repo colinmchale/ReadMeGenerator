ef02200a02baaec4d7d62b7b7e25ddb88d608182
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-12-03 22:18:11 -0600

    added validation to some prompts, remade the init function on index js

---
+++ ExampleREADME.md
@@ -0,0 +1,36 @@
+# dgbsrnsm
+[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+
+## Description  
+wrnythnrtb  
+rnwtmnyjmyk,  
+enqteqbrv  
+rbtymryi,
+
+## Table of Contents  
+[Description](#description)   
+[Installation](#installation)  
+[Usage](#usage)  
+[License](#license)  
+[Contributing](#contributing)  
+[Tests](#tests)  
+[Questions](#questions)  
+
+## Installation  
+lavarevthtym
+
+## Usage  
+etanymeuym
+
+## License  
+[Link to License](https://unlicense.org/)
+
+## Contributing  
+aefqbryneutmy
+
+## Tests  
+undefined
+
+## Questions
+Link to github: [Click Here](https://github.com/rgrtnwtumy)  
+Please submit any additional questions you may have in an email to nteunyh
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown')
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = () => {
@@ -10,21 +10,68 @@ return inquirer.prompt([
       type: 'input',
       message: "Please enter a Title:",
       name: 'title',
+      validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter your project title!');
+            return false; 
+        }
+      }
     },
     {
       type: 'input',
-      message:  "Please provide a description of the application:",
-      name: 'description',
+      message:  "What was your motivation for the application?",
+      name: 'motivation',
+      validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter the reason for creating this application!');
+            return false; 
+        }
+      }
     },
     {
       type: 'input',
-      message:  "Please enter installation instructions:",
+      message:  "Why did you build this project?",
+      name: 'why',
+    },
+    {
+      type: 'input',
+      message:  "What problem does it solve?",
+      name: 'solution',
+    },
+    {
+      type: 'input',
+      message:  "What did you learn?",
+      name: 'lesson',
+    },
+    {
+      type: 'input',
+      message:  "Please enter steps required to install project:",
       name: 'installation',
+      validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter installation steps!');
+            return false; 
+        }
+      }
     },
     {
       type: 'input',
       message:  "Please enter usage information:",
       name: 'usage',
+      validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter usage info!');
+            return false; 
+        }
+      }
     },
     {
       type: 'list',
@@ -34,71 +81,88 @@ return inquirer.prompt([
     },
     {
       type: 'input',
-      message:  "Please enter contribution guidelines:",
+      message:  "Please enter guidelines for how other programmers can contribute:",
       name: 'guidelines',
     },
     {
       type: 'input',
-      message: "Please enter test instructions:",
+      message: "Please enter test instructions for how to run application:",
       name: 'test',
+      validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter instrcutions!');
+            return false; 
+        }
+      }
     },
     {
       type: 'input',
-      message: "What is your github username?",
+      message: "What is your Github username?",
       name: 'username',
+      validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false; 
+        }
+      }
     },
     {
       type: 'input',
       message: "What is your email?",
       name: 'email',
+      validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter your email!');
+            return false; 
+        }
+      }
     },
   ])
 };
 
 // TODO: Create a function to write README file
-function readMeGenerator(response) {
-    return `# ${response.title}
+function writeToFile(data) {
+    fs.writeFile("ExampleREADME.md", data, err => {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Your README.md file has been generated")
+    });
+}
 
-## Table of Contents  
-[Description](#Description)   
-[Installation](#Installation)  
-[Usage](#Usage)  
-[License](#License)  
-[Contributing](#Contributing)  
-[Tests](#Tests)  
-[Questions](#Questions)  
-
-## Description  
-${response.description}
-
-## Installation  
-${response.installation}
-
-## Usage  
-${response.usage}
-
-## License  
-${response.license}
-
-## Contributing  
-${response.guidelines}
-
-## Tests  
-${response.tests}
-
-## Questions  
-Link to github: [Click Here](https://github.com/${response.username})  
-Please submit any additional questions you may have in an email to ${response.email}
-`   
-};
 
 // TODO: Create a function to initialize app
 function init() {
+
   questions()
-    // Use writeFileSync method to use promises instead of a callback function
-    .then((response) => fs.writeFileSync("README.md", readMeGenerator(response)))
-    .then(() => console.log('Successfully wrote to README.md'))
-    .catch((err) => console.error(err));
+// Get the answers from user 
+.then(answers => {
+  console.log("answers: " + answers)
+    return generateMarkdown(answers);
+})
+// Use data to display on the ReadMe 
+.then(data => {
+  console.log("data: " + data)
+    return writeToFile(data);
+})
+// Catch errors
+.catch(err => {
+    console.log(err)
+})
+
+
+  // questions()
+  //   // Use writeFileSync method to use promises instead of a callback function
+  //   .then((data) => fs.writeFileSync("ExampleREADME.md", generateMarkdown))
+  //   .then(() => console.log('Successfully wrote to README.md'))
+  //   .catch((err) => console.error(err));
+    
 }
 
 // Function call to initialize app
